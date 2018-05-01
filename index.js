1b597851d7646efd9e20e417f9808cb2f6389df2
yacine.farhat@esprit.tn <yacine.farhat@esprit.tn>
2018-05-01 02:57:28 +0100

    Cleaning unused files

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-require('./models/User');
-require('./models/Survey');
-require('./services/passport');
+
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
@@ -14,18 +11,11 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(bodyParser.json());
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
 
-require('./routes/authRoutes')(app);
-require('./routes/billingRoutes')(app);
-require('./routes/surveyRoutes')(app);
+
+
+
+
 require('./routes/productsRoutes')(app);
 require('./routes/carsRoutes')(app);
 
